2a9a030aae661a1057177f2b4e7ac0f78b980a6e
Artem <arpushkarev@gmail.com>
2023-02-14 15:23:27 +0100

    added db to create

--- Makefile
+++ Makefile
@@ -5,3 +5,22 @@ generate:
 				--go_out=pkg/note_v1 --go_opt=paths=source_relative \
 				--go-grpc_out=pkg/note_v1 --go-grpc_opt=paths=source_relative \
 				api/note_v1/service.proto
+
+LOCAL_MIGRATION_DIR=./migrations
+LOCAL_MIGRATION_DSN="host=localhost port=54321 dbname=note-service user=note-service-user password=note-service-password sslmode=disable"
+
+.PHONY: install-goose
+.install-goose:
+	go install github.com/pressly/goose/v3/cmd/goose@latest
+
+.PHONY: local-migration-status
+local-migration-status:
+	goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} status -v
+
+.PHONY: local-migration-up
+local-migration-up:
+	goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} up -v
+
+.PHONY: local-migration-down
+local-migration-down:
+	goose -dir ${LOCAL_MIGRATION_DIR} postgres ${LOCAL_MIGRATION_DSN} down -v
--- internal/app/api/note_v1/create.go
+++ internal/app/api/note_v1/create.go
@@ -4,17 +4,60 @@ import (
 	"context"
 	"fmt"
 
+	sq "github.com/Masterminds/squirrel"
 	desc "github.com/arpushkarev/note-service-api/pkg/note_v1"
+	_ "github.com/jackc/pgx/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+	noteTable  = "note"
+	host       = "localhost"
+	port       = "54321"
+	dbUser     = "note-service-user"
+	dbPassword = "note-service-password"
+	dbName     = "note-service"
+	sslMode    = "disable"
 )
 
 func (n *Implementation) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	fmt.Println("CreateNote")
-	fmt.Println("title:", req.GetTitle())
-	fmt.Println("text:", req.GetText())
-	fmt.Println("author:", req.GetAuthor())
+	dbDsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, dbUser, dbPassword, dbName, sslMode,
+	)
+
+	db, err := sqlx.Open("pgx", dbDsn)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	builder := sq.Insert(noteTable).
+		PlaceholderFormat(sq.Dollar).
+		Columns("title, text, author").
+		Values(req.GetTitle(), req.GetText(), req.GetAuthor()).
+		Suffix("returning id")
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	row, err := db.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer row.Close()
+
+	row.Next()
+	var id int64
+	err = row.Scan(&id)
+	if err != nil {
+		return nil, err
+	}
 
 	return &desc.CreateResponse{
-		Id: 1,
+		Id: id,
 	}, nil
 
 }
